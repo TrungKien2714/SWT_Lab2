86816e6ccbcfe844e7d5a187debbf86ac44a84a4
lumuwumul <5chhoang5@gmail.com>
2025-03-11 15:02:00 +0700

    lab2 lam test

--- src/java/controller/adminController/EditAccount.java
+++ src/java/controller/adminController/EditAccount.java
@@ -13,7 +13,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.UserDetail;
 
 /**
@@ -97,7 +100,11 @@ public class EditAccount extends HttpServlet {
 
         DAOforAdmin acc_dao = new DAOforAdmin();
 
-        acc_dao.editAcc(fullname, phonenumber, address, role, id);
+        try {
+            acc_dao.editAcc(fullname, phonenumber, address, role, id);
+        } catch (SQLException ex) {
+            Logger.getLogger(EditAccount.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         session.setAttribute("email", null);
         session.setAttribute("id", null);
